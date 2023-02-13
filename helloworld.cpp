055b1618f1ac124eab5e811426e51adfcb9cfa8a
khussey45 <khussey45@gmail.com>
2023-02-13 10:19:42 -0500

    fagfg

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,11 +2,27 @@
 
 int main()
 {
-    // type conversion = converting the value of one data type to another
-    // Implicit = automatic
-    // Explicit = Precede value with new data type
+    // cout << (insertion operator)
+    // cin >> (extraction operator)
 
-    int
+    std::string name;
+    int age;
 
+    std::cout << "whats your age?: ";
+    std::cin >> age;
+
+    std::cout << "What's your full name?: ";
+    std::getline(std::cin >> std::ws, name);
+
+    
+
+    std::cout << "hello " << name << '\n';
+    std::cout << "You are " << age << " years old";
+
+   
     return 0;
 }
+
+// type conversion = converting the value of one data type to another
+    // Implicit = automatic
+    // Explicit = Precede value with new data type
