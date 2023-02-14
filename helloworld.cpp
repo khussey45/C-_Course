cb80d3317a2ff71877293d6b416da5e2e5c6c66f
khussey45 <khussey45@gmail.com>
2023-02-14 12:50:35 -0500

    tads

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,39 +4,41 @@
 
 int main()
 {
-    // TEMPERATURE CONVERSION
-    double temp;
-    char unit;
+    
+    
 
-    std::cout << "***** TEMPERATURE CONVERSION ******\n";
-    std::cout << "F = Fehrenheit\n";
-    std::cout << "C = Celsius\n";
-    std::cout << "What unit would you like to convert to: ";
-    std::cin >> unit;
-
-    if(unit == 'F' || unit == 'f'){
-        std::cout << "Enter the temperature in Celsius: ";
-        std::cin >> temp;
-
-        temp = (1.8 * temp + 32.0);
-        std::cout << "Temperature is: " << temp << "degF\n";
-    }
-    else if(unit == 'C' || unit == 'c') {
-        std::cout << "Enter the temperature in Fahrenhit: ";
-        std::cin >> temp;
-
-        temp = (temp - 32) / 1.8;
-        std::cout << "Temperature is: " << temp << "degC\n";
-    } 
-
-    std::cout << "*******************************";
     
    
     return 0;
 }
 
 
+// TEMPERATURE CONVERSION
+    // double temp;
+    // char unit;
 
+    // std::cout << "***** TEMPERATURE CONVERSION ******\n";
+    // std::cout << "F = Fehrenheit\n";
+    // std::cout << "C = Celsius\n";
+    // std::cout << "What unit would you like to convert to: ";
+    // std::cin >> unit;
+
+    // if(unit == 'F' || unit == 'f'){
+    //     std::cout << "Enter the temperature in Celsius: ";
+    //     std::cin >> temp;
+
+    //     temp = (1.8 * temp + 32.0);
+    //     std::cout << "Temperature is: " << temp << "degF\n";
+    // }
+    // else if(unit == 'C' || unit == 'c') {
+    //     std::cout << "Enter the temperature in Fahrenhit: ";
+    //     std::cin >> temp;
+
+    //     temp = (temp - 32) / 1.8;
+    //     std::cout << "Temperature is: " << temp << "degC\n";
+    // } 
+
+    // std::cout << "*******************************";
  // // CALCULATOR PROGRAM
 
     // char op;
