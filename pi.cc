694ab1fe2766ff6e47fda645b25063cb485ee633
Kieren James Hussey <khussey45@gmail.com>
2023-08-07 13:50:10 -0400

    clang++

--- pi.cc
+++ pi.cc
@@ -3,10 +3,13 @@ using namespace std;
 
 const double pi = 3.141593;
 
-int main() 
-{
- const double radius = 1.5;
+int main() {
+  const double radius = 1.5;
   const double area = pi * radius * radius;
   cout << area;
 
-}
+} 
+
+
+
+
