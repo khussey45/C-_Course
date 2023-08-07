1773406ba9a69acba7c3539b168b471a3ba463f6
Kieren James Hussey <khussey45@gmail.com>
2023-08-06 23:45:51 -0400

    Cleaned up code a bit.

--- pi.cc
+++ pi.cc
@@ -1,11 +1,12 @@
 #include <iostream>
+using namespace std;
 
-double pi = 3.141593;
+const double pi = 3.141593;
 
 int main() 
 {
-  double area, radius = 1.5;
-  area = pi * radius * radius;
-  std::cout << area;
+ const double radius = 1.5;
+  const double area = pi * radius * radius;
+  cout << area;
 
 }
